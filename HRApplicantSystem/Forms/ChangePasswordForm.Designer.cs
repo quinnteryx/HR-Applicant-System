34692b804bea65df8711ad7db582839a25e6c56b
Andreii <andreicastillo200611@gmail.com>
2026-06-07 09:08:52 +0800

    Added ChangePasswordForm and HRLoginForm - Castillo :v

---
+++ HRApplicantSystem/Forms/ChangePasswordForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace HRApplicantSystem.Forms
+{
+    partial class ChangePasswordForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.txtCurrent = new System.Windows.Forms.TextBox();
+            this.lblNew = new System.Windows.Forms.Label();
+            this.txtNew = new System.Windows.Forms.TextBox();
+            this.lblConfirm = new System.Windows.Forms.Label();
+            this.txtConfirm = new System.Windows.Forms.TextBox();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblTitle.Location = new System.Drawing.Point(12, 33);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(202, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Change Password";
+            // 
+            // lblCurrent
+            // 
+            this.lblCurrent.AutoSize = true;
+            this.lblCurrent.Location = new System.Drawing.Point(14, 85);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(90, 13);
+            this.lblCurrent.TabIndex = 1;
+            this.lblCurrent.Text = "Current Password";
+            // 
+            // txtCurrent
+            // 
+            this.txtCurrent.Location = new System.Drawing.Point(17, 101);
+            this.txtCurrent.Name = "txtCurrent";
+            this.txtCurrent.PasswordChar = '*';
+            this.txtCurrent.Size = new System.Drawing.Size(300, 20);
+            this.txtCurrent.TabIndex = 2;
+            // 
+            // lblNew
+            // 
+            this.lblNew.AutoSize = true;
+            this.lblNew.Location = new System.Drawing.Point(14, 140);
+            this.lblNew.Name = "lblNew";
+            this.lblNew.Size = new System.Drawing.Size(78, 13);
+            this.lblNew.TabIndex = 3;
+            this.lblNew.Text = "New Password";
+            // 
+            // txtNew
+            // 
+            this.txtNew.Location = new System.Drawing.Point(17, 156);
+            this.txtNew.Name = "txtNew";
+            this.txtNew.PasswordChar = '*';
+            this.txtNew.Size = new System.Drawing.Size(300, 20);
+            this.txtNew.TabIndex = 4;
+            // 
+            // lblConfirm
+            // 
+            this.lblConfirm.AutoSize = true;
+            this.lblConfirm.Location = new System.Drawing.Point(14, 195);
+            this.lblConfirm.Name = "lblConfirm";
+            this.lblConfirm.Size = new System.Drawing.Size(116, 13);
+            this.lblConfirm.TabIndex = 5;
+            this.lblConfirm.Text = "Confirm New Password";
+            // 
+            // txtConfirm
+            // 
+            this.txtConfirm.Location = new System.Drawing.Point(17, 211);
+            this.txtConfirm.Name = "txtConfirm";
+            this.txtConfirm.PasswordChar = '*';
+            this.txtConfirm.Size = new System.Drawing.Size(300, 20);
+            this.txtConfirm.TabIndex = 6;
+            // 
+            // btnChange
+            // 
+            this.btnChange.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnChange.ForeColor = System.Drawing.Color.White;
+            this.btnChange.Location = new System.Drawing.Point(17, 255);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(150, 35);
+            this.btnChange.TabIndex = 7;
+            this.btnChange.Text = "Change Password";
+            this.btnChange.UseVisualStyleBackColor = false;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(175, 255);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(150, 35);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // ChangePasswordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(404, 341);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.txtConfirm);
+            this.Controls.Add(this.lblConfirm);
+            this.Controls.Add(this.txtNew);
+            this.Controls.Add(this.lblNew);
+            this.Controls.Add(this.txtCurrent);
+            this.Controls.Add(this.lblCurrent);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "ChangePasswordForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change Password";
+            this.Load += new System.EventHandler(this.ChangePasswordForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblCurrent;
+        private System.Windows.Forms.TextBox txtCurrent;
+        private System.Windows.Forms.Label lblNew;
+        private System.Windows.Forms.TextBox txtNew;
+        private System.Windows.Forms.Label lblConfirm;
+        private System.Windows.Forms.TextBox txtConfirm;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ HRApplicantSystem/Forms/ChangePasswordForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using HRApplicantSystem.Database;
+using HRApplicantSystem.Classes;
+
+namespace HRApplicantSystem.Forms
+{
+    public partial class ChangePasswordForm : Form
+    {
+        public ChangePasswordForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            string current = txtCurrent.Text.Trim();
+            string newPass = txtNew.Text.Trim();
+            string confirm = txtConfirm.Text.Trim();
+
+            if (current == "" || newPass == "" || confirm == "")
+            {
+                MessageBox.Show("Please fill in all fields.", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (newPass != confirm)
+            {
+                MessageBox.Show("New passwords do not match.", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (newPass.Length < 6)
+            {
+                MessageBox.Show("New password must be at least 6 characters.", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (current == newPass)
+            {
+                MessageBox.Show("New password must be different from current password.", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            OleDbConnection conn = DBConnection.GetConnection();
+            if (conn == null) return;
+
+            try
+            {
+                conn.Open();
+
+                string table = UserSession.Role == "Applicant" ?
+                               "ApplicantAccounts" : "Users";
+                string idColumn = UserSession.Role == "Applicant" ?
+                                  "ApplicantAccountID" : "UserID";
+
+                string verifyQuery = "SELECT COUNT(*) FROM " + table +
+                                     " WHERE " + idColumn + "=@id" +
+                                     " AND PasswordHash=@current";
+                OleDbCommand verifyCmd = new OleDbCommand(verifyQuery, conn);
+                verifyCmd.Parameters.AddWithValue("@id", UserSession.UserID);
+                verifyCmd.Parameters.AddWithValue("@current", current);
+                int match = Convert.ToInt32(verifyCmd.ExecuteScalar());
+
+                if (match == 0)
+                {
+                    MessageBox.Show("Current password is incorrect.", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    conn.Close();
+                    return;
+                }
+
+                string updateQuery = "UPDATE " + table +
+                                     " SET PasswordHash=@newPass" +
+                                     " WHERE " + idColumn + "=@id";
+                OleDbCommand updateCmd = new OleDbCommand(updateQuery, conn);
+                updateCmd.Parameters.AddWithValue("@newPass", newPass);
+                updateCmd.Parameters.AddWithValue("@id", UserSession.UserID);
+                updateCmd.ExecuteNonQuery();
+
+                conn.Close();
+
+                MessageBox.Show("Password changed successfully!",
+                    "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ChangePasswordForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
